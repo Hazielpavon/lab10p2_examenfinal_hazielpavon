6b6b3fa0bf141783d7ca7b3edf29093d99c2455e
Haziel Pavon <hazielpavon93@gmail.com>
2023-09-22 16:17:49 -0600

    Clases terminadas

--- src/lab10p2_examenfinal_hazielpavon/Main.java
+++ src/lab10p2_examenfinal_hazielpavon/Main.java
@@ -509,9 +509,7 @@ public class Main extends javax.swing.JFrame {
 
     }//GEN-LAST:event_jButton2MouseClicked
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
