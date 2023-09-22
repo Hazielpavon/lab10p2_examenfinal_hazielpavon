c55c7b1c6830c270742ff6d14c19c054dc2734de
Haziel Pavon <hazielpavon93@gmail.com>
2023-09-22 13:03:59 -0600

    Iniciando

---
+++ src/lab10p2_examenfinal_hazielpavon/Lab10P2_ExamenFinal_HazielPavon.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab10p2_examenfinal_hazielpavon;
+
+/**
+ *
+ * @author hapav
+ */
+public class Lab10P2_ExamenFinal_HazielPavon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
